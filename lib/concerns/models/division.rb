df33f4a38d4de93fe1a685cd7f7044d3c2a604bb
Jason Ronallo <jronallo@gmail.com>
2014-06-09 14:07:19 -0400

    scopes are defined differently in rails 4.1

--- lib/concerns/models/division.rb
+++ lib/concerns/models/division.rb
@@ -2,10 +2,10 @@ module ScamsModels::Concerns::Models::Division
   extend ActiveSupport::Concern
 
   included do
-    belongs_to :resource  
+    belongs_to :resource
     belongs_to :division_type
     has_many :pages, :dependent => :destroy
-    default_scope :order => 'divisionSequenceNumber ASC'
+    default_scope { order('divisionSequenceNumber ASC') }
   end
 
 end
