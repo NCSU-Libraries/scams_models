57bd14e0ac1057400e802b24626eb4f208405e4a
Kevin Beswick <kdbeswic@ncsu.edu>
2021-11-29 14:59:42 -0500

    rails 6 compatibility for new sal

--- lib/scams_models/engine.rb
+++ lib/scams_models/engine.rb
@@ -2,7 +2,13 @@ module ScamsModels
   class Engine < Rails::Engine
     engine_name :scams_models
     initializer "establish_scams_models_connections" do
-      if Rails.application.class.parent_name != "Scams"
+      if Rails::VERSION::MAJOR >= 6
+        parent_class_name = Rails.application.class.module_parent_name
+      else
+        parent_class_name = Rails.application.class.parent_name
+      end
+
+      if parent_class_name != "Scams"
         # FIXME: more types need to be added here for this to work in
         # different apps
         [ScamsModelBase, AssetType,Bitstream,Bundle,Classification,Contributor,Creator,Crop,DigitalCreator,
